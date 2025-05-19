ced1df6da57647dc51a636f66edac4f6ba99dca7
Folayan Ayodeji <foladeji2000@yahoo.com>
2025-05-18 23:02:50 -0400

    Update task_scheduling.sql

--- task_scheduling.sql
+++ task_scheduling.sql
@@ -1,3 +1,8 @@
+-- change context
+use role sysadmin;
+use schema dev_db.stage_sch;
+use warehouse adhoc_wh;
+
 create or replace table completed_orders(
 order_id int,
 product varchar(20),
@@ -14,6 +19,8 @@ warehouse = COMPUTE_WH
 SCHEDULE = 'USING CRON */2 * * * * UTC' -- every 2 mins
 as 
 INSERT INTO completed_orders SELECT * FROM orders_data_lz WHERE order_status = 'Shipped';
+ON_ERROR = ABORT_STATEMENT; 
+
 
 alter task target_table_ingestion suspend; -- by default a task is automatically set to false
 
@@ -25,6 +32,12 @@ warehouse = COMPUTE_WH
 after target_table_ingestion
 as 
 delete from  completed_orders WHERE ORDER_DATE < current_date();
+
+
+use role accountadmin; -- to be able to manage tasks 
+grant execute task, execute managed task on account to role sysadmin;
+use role sysadmin;
+
 
 alter task target_table_ingestion_next suspend; 
 
